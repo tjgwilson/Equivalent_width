126ad38d80852a681c44a063977e2b692ba62f1e
Thomas Wilson <twilson@wasabiastro.local>
2019-01-17 16:52:37 +0000

    Initial files

---
+++ equivalant_width.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy as scp
+import matplotlib.pyplot as plt
+
+
+
+class equivilant_width:
+
+    def __init__(self,input_file):
+        self.input_file = input_file
+
+
+    def load_data(self)
+        
